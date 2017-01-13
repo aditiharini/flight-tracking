7b911c2b969a2ecec202e56dad6c4e94b9e65b2e
Neha Prasad <nehaprasad@Nehas-MacBook-Pro.local>
2017-01-13 00:46:31 -0500

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request'); 
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/scrape', function(req, res, next) {
+
+	request.post('http://www.britishairways.com/travel/home/public/en_gb', {
+		form: {
+		depCountry: 'United Kingdom',
+		from: 'London', 
+		journeyTypeRT: 'on', 
+		to: 'Hyderabad',
+		depDate: '12/01/17', 
+		retDate: '1/01/17', 
+		cabin: 'Economy', 
+		restrictionType: 'Lowest price', 
+		ad: '1', 
+		ya: '0', 
+		ch: '0', 
+		inf: '0', 
+		getFlights: 'Find Flights'
+		}
+	}, function (error, response, body){
+		if (error) {
+			console.log ("Submission failed"); 
+		} else { 
+			console.log(body); 
+		}
+		
+	})
+}); 
+
+module.exports = router; 
